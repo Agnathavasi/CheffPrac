5cfa99a2e0c7ea6a829c0056fd41a7557a5a9f49
root <root@mxtdbserver.mxtvalidator2>
2018-07-30 09:14:22 +0000

    cleaning

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,27 @@
+package 'ntp' do
+  action :remove
+end
+
+package 'tree' do
+  action :remove
+end 
+
+package 'tree' do
+     action :install
+end
+
+package 'ntp' do
+   action :install
+end
+
+file '/Chef/hellotest.txt' do
+    content 'Hello testing'
+    owner 'root'
+    group 'root'
+    mode  '777'
+end
+
+file '/etc/motd' do
+   content 'This is the chef file'
+end
+
